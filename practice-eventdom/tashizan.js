d7c35d07c754b978934f06608ccbc32117600c18
G484862024 <248486@st.takushoku-u.ac.jp>
2025-07-17 16:27:50 +0900

    jsの追加と課題4-10

--- practice-eventdom/tashizan.js
+++ practice-eventdom/tashizan.js
@@ -1,4 +1,14 @@
 function tashizan(){
 
-    let i=
+    let x=document.querySelector('input[name=left]');
+    left Number=(x.value);
+    let y=document.querySelector('input [name=right]');
+    right Number=(y.value);
+    let answer=left+right;
+    console.log('答え'+answer);
+    let z=document.querySelector('span#answer');
+    z.textContent=answer;
+
 }
+let btn=document.querySelector('button#calc');
+btn.addEventListener('click',tashizan);
