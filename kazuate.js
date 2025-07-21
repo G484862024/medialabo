943155541d4616fde3c98dbeaddabec87f36eab6
G484862024 <248486@st.takushoku-u.ac.jp>
2025-07-21 21:55:01 +0900

    数当てゲーム修正

--- kazuate.js
+++ kazuate.js
@@ -19,27 +19,33 @@ function hantei(){
     yoso=Number(a.value);
     kaisu=kaisu+1;
     console.log(kaisu+'回目の予想:'+yoso);
-    if(kaisu>3){
-        console.log('答えは'+kotae+'でした.既にゲームは終わっています');
-    }
-    if (kaisu<=3){
-        if(yoso===kotae){
-            console.log('正解です.おめでとう!');
-            kaisu=kaisu+3;
-        }else{
-            if(kaisu===3){
-                console.log('まちがい.残念でした答えは'+kotae+'です.');
-            }else if(kaisu<3){
-                if(yoso<kotae){
-                console.log('まちがい.答えはもっと大きいですよ.');
-                }if(yoso>kotae){
-                console.log('まちがい.答えはもっと小さいですよ.');
-                }
-            }
-        }
-    }
+    let b=document,querySelector('span#kaisu');
+    b.textContent=yoso;
+    let c=document.querySelector('p#result');
+
+
 
     //課題3-1:正解判定する
     //kotaeとyosoが一致するかどうか調べて結果を出力
     //課題3-1における出力先はコンソール
+    
+    
+    if(kaisu<3){
+        if(yoso==kotae){
+            console.log('正解。おめでとう');
+            c.textContent='正解。おめでとう';
+        }else if(yoso<kotae){
+            console.log('間違い。答えはもっと大きいです');
+            c.textContent='間違い。答えはもっと大きいです';
+        }else if(yoso>kotae){
+            console.log('間違い。答えはもっと小さいです');
+            c.textConsole('間違い。答えはもっと小さいです');
+        }
+    }else if(kaisu===3){
+        console.log('間違い。答えは'+kotae+'でした');
+        c.textConsole='間違い。答えは'+kotae+'でした';
+    }else{
+        console.log('答えは'+kotae+'でした。ゲームは既に終了しています');
+        c.textContent='答えは'+kotae+'でした。ゲームは既に終了しています';
+    }
 }
